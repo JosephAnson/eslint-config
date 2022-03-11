d7d9fce552a0b2d727a50f4ba22e105ba7bf0bce
Joseph Anson <josephanson@hotmail.co.uk>
2022-03-11 12:52:07 +0100

    feat: update typescript rules

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -42,13 +42,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-parameter-properties': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/ban-ts-ignore': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/no-namespace': 'off',
   },
 }
